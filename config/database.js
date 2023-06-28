5499b238d33d455404d5b2bd4cff0707bfec3485
BhaveshKumar <bk769221@gmail.com>
2023-06-28 22:21:13 +0530

    roxiler-backend

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connectToMongo = mongoose.connect(process.env.MONGO_URL, {
+const connectToMongo = mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
